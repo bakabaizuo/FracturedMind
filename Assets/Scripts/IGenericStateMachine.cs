ebf9aebfe1247d25853cb103eed2f23861b50268
Shawn <shawn_aberin@dlsu.edu.ph>
2025-07-26 10:25:41 +0800

    Gave IGenericStateMachine array for states and currentState

--- Assets/Scripts/IGenericStateMachine.cs
+++ Assets/Scripts/IGenericStateMachine.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 //String is the current type for states.
 //O is the type of expected output
-public interface IGenericStateMachine<T>{
+//S is the type of States or its Accessor
+public interface IGenericStateMachine<O,S>{
   
-  //<summary> get Machine state <\summary>
-  public string getState();
-  
+  S[] states{
+    get;
+    set;
+  }
+  S currentState{
+    get;
+  }
+
   //<summary> change machine state <\summary>
-  private string transitionState();
+  protected S transitionState();
   /*
    * If State transition needs input, do any of the following:
    *      1. Catch it with a function
@@ -29,6 +35,6 @@ public interface IGenericStateMachine<T>{
    * */
 
   //<summary> Do whatever state entails <\summary>
-  public T doState();
+  public O doState();
   //override doState required input if you want to do a mealy type of FSM.
 }
