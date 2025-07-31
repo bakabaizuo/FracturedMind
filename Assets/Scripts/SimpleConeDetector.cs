baee688fd4359d781581e5796d6c7b7983af75c9
Shawn <shawn_aberin@dlsu.edu.ph>
2025-07-31 18:59:36 +0800

    started a simple view cone detector

---
+++ Assets/Scripts/SimpleConeDetector.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleConeDetector : MonoBehaviour
+{
+    private Vector3 position;
+    private Vector3 facing;
+    private float halfXFOV;
+    private float halfYFOV;
+    private float range;
+    private float squareRange;
+    private float viewAngle;
+    public bool detectSingle(Vector3 targetPosition){
+      Vector3 offset = position-targetPosition;
+      float targetDistance = Mathf.Sqrt(Vector3.SqrMagnitude(offset));
+      //use an actual normalization if this causes problems
+      float visibility = Vector3.Dot(facing.normalized,offset*(1/targetDistance));
+      return targetDistance <= range && 
+              Mathf.Cos(halfXFOV)< visibility 
+              && Mathf.Cos(halfYFOV) < visibility; 
+
+    }    
+
+  // Start is called before the first frame update
+    void Start()
+    {
+      range = 2;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
