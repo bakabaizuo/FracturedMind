36de9b6c848dad77d7b04502a5b16ba559847570
Shawn <shawn_aberin@dlsu.edu.ph>
2025-08-26 15:08:20 +0800

    Revert "Using package with NativeArray"
    This reverts commit bbef6cd43ca309816fb06d00daeca265abe40f1a.

--- Assets/Scripts/Components/SimpleConeDetector.cs
+++ Assets/Scripts/Components/SimpleConeDetector.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Entities;
 using Unity.Mathematics;
-using Unity.Collections;
-
 public struct SimpleConeDetector: IComponentData
 {
 //    private float halfXFOV;
