bcb47b43cf1e00afa2b47ebd81ce00cadd329c02
Shawn <shawn_aberin@dlsu.edu.ph>
2025-08-26 18:09:03 +0800

    working on ecs

--- Assets/Scripts/Components/SimpleConeDetector.cs
+++ Assets/Scripts/Components/SimpleConeDetector.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Collections;
+//TODO: TURN INTO AN ARCHETYPE
 public struct SimpleConeDetector: IComponentData
 {
+
 //    private float halfXFOV;
 //    private float halfYFOV;
     public quaternion front;
