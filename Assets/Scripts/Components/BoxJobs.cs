4df0b3be70c934327853db46b5c335e07b86dc33
BrownieMayTrabaho <shawnemmanuelaberin@gmail.com>
2025-09-12 14:42:27 +0800

    Now gets a NativeArray from baked DynamicBuffers for The various collission commands

--- Assets/Scripts/Components/BoxJobs.cs
+++ Assets/Scripts/Components/BoxJobs.cs
@@ -7,15 +7,17 @@ using Unity.Burst;
 //TODO:Decide if making it generic is a good idea and won't cause funny problems later
 public struct BoxJobs:IBufferElementData{
  
-  public OverlapBoxCommand viewBox;
-    
-
+    public OverlapBoxCommand viewBox;
+    public static implicit operator OverlapBoxCommand(BoxJobs b)=> b.viewBox;
+    public static implicit operator BoxJobs(OverlapBoxCommand b) => new BoxJobs{viewBox = b};
 }
 
 public struct BoxHits:IBufferElementData{
  
   public ColliderHit hits;
+  public static implicit operator ColliderHit(BoxHits b) => b.hits;
     
+    public static implicit operator BoxHits(ColliderHit b) => new BoxHits{hits = b};
 
 }
 
