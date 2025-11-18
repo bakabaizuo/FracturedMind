fae52da176fdcbed7268882e5ba2776a427d29bf
BrownieMayTrabaho <shawnemmanuelaberin@gmail.com>
2025-11-18 18:54:03 +0800

    using scriptable objects for npc vision properties

---
+++ Assets/Scripts/Ai_Scripts/BaseEnemyConfig.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+[CreateAssetMenu(menuName = "NPC Configuration/Enemy Configuration/Base Enemy",fileName="BaseEnemyConfiguration")]
+
+sealed public class EnemyConfiguration: ScriptableObject{
+  public NPCVision visionSettings;
+  public EnemyAttention attentionSettings;
+
+  public NPCVisionBehavior npcTargets;
+
+
+}
+[CreateAssetMenu(menuName = "NPC Configuration/Enemy Configuration/Base Enemy Vision",fileName="BaseEnemyVision")]
+ sealed public class NPCVision : ScriptableObject{
+   [field:SerializeField]
+    public  float maxViewDistance{get; private set;}
+   [field:SerializeField]
+    public  float minViewDistance{get; private set;}
+   [field:SerializeField]
+    public  float fovCosTheta{get; private set;}
+   [field:SerializeField]
+    public  float eyeHeight{get; private set;}
+   [field:SerializeField]
+    public  float crouchModifier{get; private set;}
+
+ }
+
+[CreateAssetMenu(menuName = "NPC Configuration/Enemy Configuration/Base Enemy Attention",fileName="BaseEnemyAttention")]
+ sealed public class EnemyAttention : ScriptableObject{
+   [field:SerializeField]
+  public int alertDuration{get; private set;}
+   [field:SerializeField]
+  public int detectDuration{get;private set;}
+ } 
+    
+[CreateAssetMenu(menuName = "NPC Configuration/Enemy Configuration/Base Enemy Vision Behavior",fileName="BaseNPCVisionBehavior")]
+sealed public class NPCVisionBehavior:ScriptableObject{
+  [SerializeField]
+  private int layerMask;
+  [SerializeField]
+  private bool hitBackFace;
+  [SerializeField]
+  private  bool hitMultipleFaces;
+  [SerializeField]
+  private QueryTriggerInteraction howTriggers;
+  private QueryParameters parameters;
+  public QueryParameters query {get => parameters; 
+   private set {
+     parameters= new(layerMask, hitMultipleFaces, howTriggers, hitBackFace);
+   }
+  }
+}
+
