12d41958b639618d61941893bc5336e78aa287ca
BrownieMayTrabaho <shawnemmanuelaberin@gmail.com>
2025-12-10 12:36:12 +0800

    Working on Radial Menu

--- Assets/Scripts/TAB/RadialMenu.cs
+++ Assets/Scripts/TAB/RadialMenu.cs
@@ -2,11 +2,9 @@ using UnityEngine;
 
 public class RadialMenu:MonoBehavior
 {
-  RadialItem[] Items;
-  float PieTheta;
-  void Start(){
-    PieTheta= 360f/Items.Length;
 
-  }
+void Start(){
+
+}
     
 }
