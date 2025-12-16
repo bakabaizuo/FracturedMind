0c5fc2503929a8946e993e6ea606bd049a4af446
BrownieMayTrabaho <shawnemmanuelaberin@gmail.com>
2025-12-16 18:08:16 +0800

    Selector now depends on RadialMenu for Items

--- Assets/Scripts/TAB/RadialMenu.cs
+++ Assets/Scripts/TAB/RadialMenu.cs
@@ -1,17 +1,28 @@
 using UnityEngine;
 using System;
+using System.Collections.Generic;
 public class RadialMenu:MonoBehaviour
 {
 GameObject Menu;
 GameObject Selector;
+public List<RadialItem> items{get; private set;}
+public float slice{ get; private set;}
 void Start(){
-   Debug.Log("STARTING TEST");
+    items = new(3);
     Menu = gameObject.transform.GetChild(0).gameObject;
     Selector =  gameObject.transform.GetChild(1).gameObject;
+    slice = MathF.PI*2f/(items?.Count??1);
+
 }
+// void UpdateChildren(){
+  // Menu.TryGetComponent();
+// }
 void Update(){
 
   bool active =Input.GetButton("Radial Menu");
+
+  // if(!active)
+  //   return;
   Menu.SetActive(active);
   Selector.SetActive(active);
   Cursor.visible = active;
--- Assets/Scripts/TAB/Selector.cs
+++ Assets/Scripts/TAB/Selector.cs
@@ -1,23 +1,25 @@
 using UnityEngine;
 using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 public class Selector : MonoBehaviour
 {
   bool test = true;
   Camera eye;
-  RadialItem[] items;
+  List<RadialItem> items;
+  Vector2 pointer;
+  GameObject parent;
+  RadialMenu menu;
+  float slice;
   void DoSomething(RadialItem item){
   }
-  Vector2 pointer;
   static float halfWidth= Screen.width*0.5f;
   static float halfHeight = Screen.height*0.5f;
   static Vector2 cartesianer = new (halfWidth,halfHeight);
+  
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
   void Select(){
-    if(items == null || items.Length == 0 )
-      if(test)
-        items = new RadialItem[3];
-      else 
-        return;
     pointer = cartesianer - (Vector2) Input.mousePosition;
     // pointer.x = halfWidth-Input.mousePosition.x;
     // pointer.y = halfHeight - Input.mousePosition.y;
@@ -25,21 +27,36 @@ public class Selector : MonoBehaviour
       return ;
     pointer.Normalize();
     float theta = MathF.Atan2(pointer.y,pointer.x) + MathF.PI * 0.5f;
-    this.transform.rotation = Quaternion.Euler(0, 0, theta * 180f/MathF.PI);
+    this.transform.rotation = Quaternion.Euler(0, 0, theta * Mathf.Rad2Deg);
     if(theta < 0){
       theta += MathF.PI*2f;
     }
-    Debug.Log($"{theta * 180f/MathF.PI} deg");
-    float slice = MathF.PI*2f/items.Length;
+    if (items?.Count < 1)
+        return;
     //TODO: do whatever you need from this:
    //item selection is clockwise starting from top right
-    int index = items.Length-(int)MathF.Floor(theta/slice) - 1;
-    Debug.Log($"ind {index}");
-    if(items.Length> index && index > 0);
+    int index = items.Count-(int)MathF.Floor(theta/slice) - 1;
+    Debug.Log(index);
+    if(items?.Count> index && index > 0);
       DoSomething(items[index]);
   }
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
   void Update(){
     Select();
+  }
+
+  void Start(){
+    parent = transform.parent.gameObject;
+    if(parent.TryGetComponent(out menu));
+    {
+      items = menu.items;
+      slice = menu.slice;
+    }
+  }
+  void OnEnable(){
+    items = menu?.items ?? null;
+    slice = menu?.slice ?? 0f;
+    Debug.Log(items?.Count);
   }
     
 }
