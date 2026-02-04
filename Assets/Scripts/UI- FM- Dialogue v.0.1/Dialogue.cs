92df4318d46185859577d081b0a0de0615bd51a0
itzliru <cooperz4real@gmail.com>
2026-02-04 09:23:52 -0600

    smallupdate

--- Assets/Scripts/UI- FM- Dialogue v.0.1/Dialogue.cs
+++ Assets/Scripts/UI- FM- Dialogue v.0.1/Dialogue.cs
@@ -8,16 +8,20 @@ public class Dialogue : MonoBehaviour
     public TextMeshProUGUI textComponent;
     public string[] lines;
     public float textSpeed;
-
+    [SerializeField] private ChapterState chapterState;
     private int index;
 
+    void Awake()
+    {
+        chapterState = new ChapterState();
+    }
     // Start is called before the first frame update
     void Start()
     {
         textComponent.text = string.Empty;
         StartDialogue();
     }
-
+  
     // Update is called once per frame
     void Update()
     {
@@ -61,6 +65,67 @@ public class Dialogue : MonoBehaviour
         else
         {
             gameObject.SetActive(false);
+       
         }
     }
+
+enum IntroDialogueComplete
+    {
+          None = 0,
+        BulliedInHallDone = 1,
+        LadderFoundDone = 2,
+        VentEnteredDone = 3,
+        LibraryEnteredDone = 4,
+        LampExplodedDone = 5,
+        BookTakenDone = 6,
+        PortalPulledDone = 7,
+    }
+
+    //Switch StateMachine for Chapters Area
+    //we use public enum IntroStage for driving the state machine
+    private IntroDialogueComplete IntroSwitch()
+    {
+      if (chapterState.CurrentChapter != ChapterId.Intro)
+        return IntroDialogueComplete.None;
+        //(Enum)int.introStage = return flag && dialogue;
+    switch ((IntroStage)chapterState.ChapterStage)
+    {       //Enum.int
+          case IntroStage.None:
+            return IntroDialogueComplete.None;
+            //Enum.int
+        case IntroStage.BulliedInHall:
+            IntroBullyHallDialogueFlags();
+            return IntroDialogueComplete.BulliedInHallDone;
+            //Enum.int
+        case IntroStage.LadderFound:
+           // IntroLadderFoundDialogueFlags();
+            return IntroDialogueComplete.LadderFoundDone;
+            //Enum.int
+        case IntroStage.VentEntered:
+           // IntroVentEnteredDialogueFlags();
+            return IntroDialogueComplete.VentEnteredDone;
+            //debugging
+        default:
+            Debug.LogWarning("Unhandled IntroStage: " + chapterState.ChapterStage);
+            return IntroDialogueComplete.None;
+    }
+
+    return IntroDialogueComplete.None;
+}
+
+
+    
+      void  IntroBullyHallDialogueFlags()
+    {
+        chapterState.CurrentChapter = ChapterId.Intro;
+        chapterState.IntroStage = 1;
+        chapterState.SetFlag("Intro_BullyHall");
+        return IntroDialogueComplete.None;
+    }
+
+
+
+
+
+
 }
